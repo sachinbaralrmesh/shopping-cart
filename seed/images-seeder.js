faa9baab5456a75af17f493b74e1a4e24332260f
sachin Baral Ramesh <sachinbaralramesh@gmail.com>
2019-02-08 16:54:46 +0100

    chages in View
    chages in View
    by sachin

--- seed/images-seeder.js
+++ seed/images-seeder.js
@@ -1,43 +1,39 @@
 var Images = require('../models/images');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/shopping', {useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/shopping', { useNewUrlParser: true });
 
 var images = [
     new Images({
-        productId: '5bc66801cd6dfb1be8aa2c47',
-    imagePath:'http://localhost:3000/images/banner1.jpg',
-}),
+        productId: '5c54d70fad281e17403c645f',
+        imagePath: 'http://localhost:3000/images/banner1.jpg',
+    }),
 
-new Images({
-    productId: '5bc66801cd6dfb1be8aa2c47',
-imagePath:'http://localhost:3000/images/banner2.jpg',
-}),
+    new Images({
+        productId: '5c54d70fad281e17403c645f',
+        imagePath: 'http://localhost:3000/images/banner2.jpg',
+    }),
 
-new Images({
-    productId: '5bc66801cd6dfb1be8aa2c47',
-imagePath:'http://localhost:3000/images/banner3.jpg',
-}),
+    new Images({
+        productId: '5c54d70fad281e17403c645f',
+        imagePath: 'http://localhost:3000/images/banner3.jpg',
+    }),
 
-new Images({
-    productId: '5bc66801cd6dfb1be8aa2c47',
-imagePath:'http://localhost:3000/images/banner4.jpg',
-})
+    new Images({
+        productId: '5c54d70fad281e17403c645f',
+        imagePath: 'http://localhost:3000/images/banner4.jpg',
+    })
 ];
-var done=0;
-for(var i=0; i< images.length; i++)
-{
-    images[i].save(function(err,result)
-    {
+var done = 0;
+for (var i = 0; i < images.length; i++) {
+    images[i].save(function(err, result) {
         done++;
-        if(done === images.length)
-        {
+        if (done === images.length) {
             exit();
         }
     });
 }
 
-function exit()
-{
+function exit() {
     mongoose.disconnect();
 }
